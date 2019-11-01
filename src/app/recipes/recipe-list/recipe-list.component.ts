0b02e0f2b7260cb8c64495a19d7e86e85bbad0d1
Con Allen <c.allen@glofox.com>
2019-11-01 17:13:30 +0000

    recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Tasty Gear', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH4_pzlbTQ-OeYGDdM6Fr8D5LB-fwsBKIkI1g3yA1TsXMRcTx9&s' )
+
+  ];
 
   constructor() { }
 
